13d88f897910c911f9a47809c8f1fe936c9ec349
injune1123 <injune1123@gmail.com>
2018-08-12 00:12:17 -0400

    Got permutations of 4 cards and operators (brutally😅)

--- twenty-four-points/cards.js
+++ twenty-four-points/cards.js
@@ -7,7 +7,7 @@ function Card (number, suit){
 
 let aSetOfCards = [];
 const numOfCards = 52;
-
+const operatorsArr = ['+', '-', '*', '/'];
 //diamonds (♦), clubs (♣), hearts (♥) and spades (♠)
 //❤️ 🍀 ♦️ ♠️
 const allFourSuits = ["diamonds", "clubs", "hearts", "spades"];
@@ -61,3 +61,51 @@ let fourRandomCards = showCards(fourRandomNumbers);
 
 // the one console.log below is for simple testing purposes
 // console.log(fourRandomCards)
+
+//Algorithm 1 - brutal
+// find out all the permuation of the calculation of 4 numbers
+// for each see whether each one of them match 24
+// print all possible solutions
+// if there is no solution, print out "no solution"
+
+// helper functions
+// get permutations
+let fourRandomCardsPermutation = [];
+
+for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+        for (var k = 0; k < 4; k++) {
+            for (var l = 0; l < 4; l++) {
+                // for permutations
+                if (j !== i &&
+                    k !== j && k !== i &&
+                    l !== i && l !== j && l !== k 
+                ) {
+                    fourRandomCardsPermutation.push([fourRandomCards[i],fourRandomCards[j],fourRandomCards[k],fourRandomCards[l]])
+                }
+            }
+        }
+    }
+}
+
+// the two console.log below are for simple testing purposes
+// console.log(fourRandomCardsPermutation)
+// console.log(fourRandomCardsPermutation.length)
+
+let operatorsPermutation = [];
+
+for (var m = 0; m < operatorsArr.length; m++) {
+    for (var n = 0; n < operatorsArr.length; n++) {
+        for (var o = 0; o < operatorsArr.length; o++) {
+            operatorsPermutation.push([operatorsArr[m],operatorsArr[n],operatorsArr[o]]);
+        }
+    }
+}
+
+// the two console.log below is for simple testing purposes
+//console.log(operatorsPermutation);
+//console.log(operatorsPermutation.length);
+
+// situtation1 = a?b?c?d => 24
+
+// let results = [];
--- twenty-four-points/planningLog.md
+++ twenty-four-points/planningLog.md
@@ -3,3 +3,10 @@
 - [ ] Use Vanilla JavaScript to create an abstraction of an array of 52 cards [ 1~13 * 4 suits (❤️ 🍀 ♦️ ♠️) ] For example: ❤️9
 
 - [ ] get 4 random cards
+
+- [ ] print out the solutions using algorithm 1 - brutal
+    Algorithm 1 - brutal
+    // find out all the combination of the 4 numbers
+    // see whether each one of them match 24
+    // print all possible solutions
+    // if there is no solution, print out "no solution"
